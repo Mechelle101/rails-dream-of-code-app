7c74dcc2b9c420a5897543a24607c2a10cb6f333
mechelle presnell <mechellenpresnell@students.abtech.edu>
2025-04-15 12:08:58 -0400

    ran rspec tests and modified course.rb with changes

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,25 @@ class Course < ApplicationRecord
   belongs_to :coding_class
   belongs_to :trimester
   has_many :enrollments
+  has_many :students, through: :enrollments #is this needed for accessing the students? i'll test it.
 
   delegate :title, to: :coding_class
+
+  def student_name_list
+    names_list = []
+    self.enrollments.each do |enrollment|
+      names_list << "#{enrollment.student.first_name} #{enrollment.student.last_name}"
+    end
+
+    names_list
+  end
+
+  def student_email_list
+    email_list = []
+    self.enrollments.each do |enrollment|
+      email_list << enrollment.student.email #accessing email through student
+    end
+  email_list
+  end
+
 end
