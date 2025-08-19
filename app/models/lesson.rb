55966a45ff892bf4ffc9fa5b49763dae1f08595e
mechelle presnell <mechellenpresnell@students.abtech.edu>
2025-08-19 14:15:39 -0400

    fixed lesson type

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,6 @@
 class Lesson < ApplicationRecord
   has_many :lessons_topics, dependant: :destroy, inverse_of: :lesson
   has_many :topics, through: :lessons_topics
-  has_many :submissions, dependant: :destroy
+  has_many :submissions, dependent: :destroy
   belongs_to :course
 end
