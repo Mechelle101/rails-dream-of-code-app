9b7b76185b05418f7390cfcdaf96cf6f052dabdd
mechelle presnell <mechellenpresnell@students.abtech.edu>
2025-08-19 15:15:19 -0400

    fixed n+1 issue

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,13 +5,22 @@ class Course < ApplicationRecord
 
   delegate :title, to: :coding_class
 
+  # def student_name_list
+  #   names_list = []
+  #   self.enrollments.each do |enrollment| 
+  #     names_list << "#{enrollment.student.first_name} #{enrollment.student.last_name}" 
+  #   end
+  #   names_list
+  # end
+
   def student_name_list
-    names_list = []
-    self.enrollments.each do |enrollment| 
-      names_list << "#{enrollment.student.first_name} #{enrollment.student.last_name}" 
-    end
-    names_list
+    enrollments
+      .includes(:student)
+      .order(:id)
+      .map { |e| "#{e.student&.first_name} #{e.student&.last_name}" }
+      .compact
   end
+
 # could do it this way
   # def student_email_list
   #   email_list = []
