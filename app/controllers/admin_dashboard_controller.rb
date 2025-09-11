6553876a1287ce46f4c97889fde17b604936b4b8
mechelle presnell <mechellenpresnell@students.abtech.edu>
2025-09-11 04:56:41 -0400

    final adjustment

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -1,14 +1,16 @@
 class AdminDashboardController < ApplicationController
   
   def index
-    today = Date.today
+    today = Time.zone.today
 
     @current_trimester = Trimester
+      .includes(courses: :coding_class)
       .where("start_date <= ?", today)
       .where("end_date >= ?", today)
       .first
 
     @upcoming_trimester = Trimester
+      .includes(courses: :coding_class)
       .where("start_date > ?", today)
       .where("start_date < ?", today + 6.months)
       .order(:start_date)
