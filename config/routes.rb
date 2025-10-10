e97e3639f058ab1527a980097693f000c80fd03d
mechelle presnell <mechellenpresnell@students.abtech.edu>
2025-10-10 11:27:29 -0400

    Implement Lesson 13 API endpoints and recreate missing spec files

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,12 @@ Rails.application.routes.draw do
   resources :courses do 
     resources :submissions, only: [:new, :create]
   end
+
+  namespace :api do
+    namespace :v1 do
+      get '/courses', to: 'courses#index'
+      get '/courses/:course_id/enrollments', to: 'enrollments#index'
+    end
+  end
+  
 end
